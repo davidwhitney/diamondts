8c638845efeb2f2f685c8e12ef1b8a43abbd96e0
David Whitney <david@davidwhitney.co.uk>
2019-11-26 23:11:29 +0000

    Diamond Kata Init.

---
+++ app/diamond.test.ts
@@ -0,0 +1,8 @@
+import { Diamond } from "./diamond";
+
+describe("Conway's game of life", () => {
+
+    it("???", () => {
+        
+    });
+});
---
+++ app/diamond.ts
@@ -0,0 +1,13 @@
+export class Diamond {
+
+    public constructor() {
+    }
+
+    private create(): void {
+        
+    }
+
+    public toString(): string[] {
+        return [];
+    }
+}
