1d29cd205a15d562d204c0dc729573f41da32f53
David Whitney <david@davidwhitney.co.uk>
2019-11-27 01:05:55 +0000

    Completed

--- app/diamond.test.ts
+++ app/diamond.test.ts
@@ -1,8 +1,62 @@
 import { Diamond } from "./diamond";
 
 describe("Diamond kata", () => {
+    it("Given A, returns only A.", () => {
+        const sut = new Diamond("A");
+        const asString = sut.toString();
+        expect(asString).toBe("A");
+    });
 
-    it("???", () => {
+    it("Given not A, first row contains A", () => {
+        const sut = new Diamond("D");        
+        expect(sut.lines[0]).toContain("A");
+    });
+
+    it("Given not A, last row contains A", () => {
+        const sut = new Diamond("D");        
+        expect(sut.lines[sut.lines.length - 1]).toContain("A");
+    });
+
+    it("Given not A, there is more than one row", () => {
+        const sut = new Diamond("D");
+        expect(sut.lines.length).toBeGreaterThan(1);
+    });
+
+    it("Given not A, supplied letter is the middle row", () => {
+        const sut = new Diamond("B");
+        expect(sut.lines[1]).toContain("B");
+    });
+
+    it("Given not A, non-A rows feature two instances of letter", () => {
+        const sut = new Diamond("B");
         
+        const chars = Array.from(sut.lines[1]);
+
+        expect(chars.filter(c=>c == "B").length).toBe(2);
+    });
+
+    it("Given not A, letters are spaced out to make a diamond", () => {
+        const sut = new Diamond("B");
+        
+        expect(sut.lines[1]).toBe("B B");
+    });
+
+    it("Given not A, pads out rows to form diamond shape", () => {
+        const sut = new Diamond("B");
+        
+        expect(sut.lines[0]).toBe(" A ");
+        expect(sut.lines[2]).toBe(" A ");
+    });
+
+    it("Demo", () => {
+        const sut = new Diamond("C");
+
+        console.log(sut.toString());
+        
+        expect(sut.lines[0]).toBe("  A  ");
+        expect(sut.lines[1]).toBe(" B B ");
+        expect(sut.lines[2]).toBe("C   C");
+        expect(sut.lines[3]).toBe(" B B ");
+        expect(sut.lines[4]).toBe("  A  ");
     });
 });
--- app/diamond.ts
+++ app/diamond.ts
@@ -1,13 +1,41 @@
+import * as os from 'os'
+
 export class Diamond {
+    private _lines: string[];
 
-    public constructor() {
+    public constructor(midpointLetter: string) {
+        this._lines = [];
+        this.create(midpointLetter);
     }
 
-    private create(): void {
-        
+    private create(midpointLetter: string): void {
+        const distance = midpointLetter.charCodeAt(0) - 65;        
+        const charIndex = Array.from(Array(distance + 1).keys());
+        const topHalf: string[] = [];
+
+        charIndex.forEach((n: number) => {
+            const char = String.fromCharCode(n + 65);
+            if(char == 'A') {
+                topHalf.push("A");
+                return;
+            }
+
+            const requiredSpaces = topHalf[topHalf.length -1].length;
+            topHalf.push(char + " ".repeat(requiredSpaces) + char);
+        });
+
+        const widestPoint = topHalf[topHalf.length -1].length;
+
+        for(const i in topHalf) {
+            const paddingEitherSide = (widestPoint - topHalf[i].length) / 2;
+            const padding = " ".repeat(paddingEitherSide);
+            topHalf[i] = padding + topHalf[i] + padding;
+        }
+
+        const reversed = topHalf.slice(0, topHalf.length-1).reverse()
+        this._lines.push(...topHalf, ...reversed);
     }
 
-    public toString(): string[] {
-        return [];
-    }
+    get lines():string[] { return this._lines; }
+    public toString = (): string => this._lines.join(os.EOL);
 }
